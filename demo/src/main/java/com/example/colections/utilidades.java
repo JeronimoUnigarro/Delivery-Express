1bc9dce4c22fd6faa6bee4461a8d5ec90d713086
JeronimoUnigarro <jeronimo.unigarro@campusuccl.edu.co>
2025-08-29 12:00:39 -0500

    archivo de pedidio

---
+++ demo/src/main/java/com/example/colections/utilidades.java
@@ -0,0 +1,5 @@
+package com.example.colections;
+
+public class utilidades {
+    
+}
---
+++ demo/src/main/java/com/example/models/pedido.java
@@ -0,0 +1,54 @@
+// archivo: src/main/java/com/example/models/pedido.java
+package com.example.models;
+
+public class pedido {
+    private String producto;
+    private int cantidad;
+    private double precio;
+
+    public pedido(String producto, int cantidad, double precio) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public double getTotal() {
+        return cantidad * precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "producto='" + producto + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                ", total=" + getTotal() +
+                '}';
+    }
+
+    // equals y hashCode para evitar duplicados en HashSet
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof pedido)) return false;
+        pedido p = (pedido) o;
+        return producto.equals(p.producto) && cantidad == p.cantidad && precio == p.precio;
+    }
+
+    @Override
+    public int hashCode() {
+        return producto.hashCode() + cantidad * 31 + (int) precio * 17;
+    }
+}
