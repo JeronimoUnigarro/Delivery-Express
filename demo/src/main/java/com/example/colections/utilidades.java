f7b95ad74ba5590ac96268288a1370ff3e1847b2
JeronimoUnigarro <jeronimo.unigarro@campusuccl.edu.co>
2025-08-29 12:16:35 -0500

    archivo utilidades

--- demo/src/main/java/com/example/colections/utilidades.java
+++ demo/src/main/java/com/example/colections/utilidades.java
@@ -1,5 +1,19 @@
 package com.example.colections;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class utilidades {
-    
+
+
+    public static <T> List<T> filtrar(List<T> lista, Predicate<T> criterio) {
+        List<T> resultado = new ArrayList<>();
+        for (T elemento : lista) {
+            if (criterio.test(elemento)) {
+                resultado.add(elemento);
+            }
+        }
+        return resultado;
+    }
 }
